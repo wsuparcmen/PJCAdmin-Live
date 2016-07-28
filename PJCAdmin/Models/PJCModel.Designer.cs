6160de0ad91b3afd76e1470a99c6f4a24826dfd9
wsuparcmen <wsuparcmen@gmail.com>
2016-07-27 20:35:31 -0600

    Changed database edmx file to included database changes

--- PJCAdmin/Models/PJCModel.Designer.cs
+++ PJCAdmin/Models/PJCModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Wayne\Desktop\Notes\Spring 2016\CS 4750\repo\PJCAdmin\Models\PJCModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\w01102718\Desktop\PJCAdmin.git\trunk\PJCAdmin\Models\PJCModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
