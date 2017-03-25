ec9169d94bf59514ee82cd768bad3f6c1aaac15f
absprague <ashtonsprague@mail.weber.edu>
2017-03-24 18:47:24 -0600

    Fixed PostRoutine

--- PJCAdmin/ControllersAPI/RoutineController.cs
+++ PJCAdmin/ControllersAPI/RoutineController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using PJCAdmin.Models;
 using PJCAdmin.Classes.Helpers;
 using PJCAdmin.Classes.Helpers.APIModelHelpers;
+using System.Web.Helpers;
+using System.Web.Http.Controllers;
 
 namespace PJCAdmin.ControllersAPI
 {
@@ -20,6 +22,7 @@ namespace PJCAdmin.ControllersAPI
             helper = new RoutineHelper();
             DbHelper context = helper.getDBHelper();
             auth = new Auth(context);
+            //string some = Request.Content.ToString();
         }
 
         // GET api/Routine?token=<token>
@@ -31,57 +34,79 @@ namespace PJCAdmin.ControllersAPI
             return helper.getAllRoutinesAssignedToUserForSerialization(userName);
         }
 
-        // GET api/Routine?token=<token>&assignedBy=<"Parent" or "Job Coach">
-        /*public IEnumerable<Routine> Get(string token, string assignedBy)
-        {
-            auth.authorizeToken(token);
-            string userName = auth.getUserNameFromToken(token);
-
-            if (assignedBy.Equals("Parent"))
-                return helper.getRoutinesAssignedByParentForSerialization(userName);
-            if (assignedBy.Equals("Job Coach"))
-                return helper.getRoutinesAssignedByJobCoachForSerialization(userName);
-
-            //assignedBy is not a valid string
-            throw new HttpResponseException(new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest));
-        }*/
-
         //GET api/Routine?token=<token>&username=<username>
         public IEnumerable<Routine> GetRoutineListForUser(string token, string username)
         {
+            if (username == null) return null;
             auth.authorizeToken(token);
             return helper.getAllRoutinesAssignedToUserForSerialization(username);
         }
-        
-        //POST api/Routine?token=<token>&create=<c,m,or d>&routineModel=<[routine object]>
-        [HttpPost]
-        public HttpResponseMessage PostJob(string token, string create, RoutineModel model)
+
+        /*[HttpGet]
+        public IEnumerable<string> GetRoutineInfo(string token, string username, string routineTitle)
         {
+            List<string> routineInfo = new List<string>();
+            if (username == null) return null;
             auth.authorizeToken(token);
+            //UserInfoModel temp = helper.getRoutineInfo(username);
+            //userInfo.Add(temp.userName);
+            //userInfo.Add(temp.phone);
+            //userInfo.Add(temp.email);
+
+            Routine temp = helper.getRoutineAssignedToByName(routineTitle);
+            routineInfo.Add(temp.routineID.ToString());
+            routineInfo.Add(temp.creatorUserName);
+            routineInfo.Add(temp.assigneeUserName);
+            routineInfo.Add(temp.routineTitle);
+            routineInfo.Add(temp.isTimed.ToString());
+            routineInfo.Add(temp.expectedDuration.ToString());
+            routineInfo.Add(temp.isNotifiable.ToString());
+            routineInfo.Add(temp.updatedDate.ToString());
+            routineInfo.Add(temp.isDisabled.ToString());
+
+            return routineInfo;
+        }*/
+
+        //POST api/Routine?token=<token>&create=<c,m,or d>&model=<[routine object]>
+        [HttpPost]
+        public HttpResponseMessage PostRoutine(PostRoutineRequest data)
+        {
+            string token = data.token;
+            string create = data.create;
+
             PJCAdmin.Classes.Helpers.MVCModelHelpers.RoutineHelper helper = new Classes.Helpers.MVCModelHelpers.RoutineHelper();
+            RoutineModel routine = Json.Decode<RoutineModel>(data.model.ToString());
+
             switch (create)
             {
                 case "c": //create new job
                     //create the model in the database
-                    helper.createRoutine(model);
+                    helper.createRoutine(auth.getUserNameFromToken(token), routine);
                     break;
 
                 case "m": //modify existing job
                     //find the routine that matches
                     //update the database for routine
-                    helper.updateRoutine(model.routineTitle, model);
+                    helper.updateRoutine(auth.getUserNameFromToken(token), routine.routineTitle, routine);
                     break;
 
                 case "d": //delete existing job
                     //find the routine that matches
                     //delete the matching routine from the database
-                    helper.deleteRoutine(model.routineTitle, auth.getUserNameFromToken(token), false);
+                    helper.deleteRoutine(routine.routineTitle, auth.getUserNameFromToken(token), false);
                     return Request.CreateResponse<string>(HttpStatusCode.OK, "Job Deleted");
 
                 default: return Request.CreateResponse<string>(HttpStatusCode.BadRequest, "Create Code Not Recognized");
             }
 
-            return Request.CreateResponse<string>(HttpStatusCode.Created, "Job Created");
+            return Request.CreateResponse<string>(HttpStatusCode.Created, "Job Created"); 
+        }
+
+        public class PostRoutineRequest
+        {
+            public string token { get; set; }
+            public string create { get; set; }
+            public object model { get; set; }
         }
 
         protected override void Dispose(bool disposing)
@@ -89,7 +114,8 @@ namespace PJCAdmin.ControllersAPI
             helper.dispose();
             base.Dispose(disposing);
         }
-
+        
+        [HttpGet]
         public RoutineModel GetRoutineInfo(string token, string username, string routineTitle)
         {
             RoutineModel routineInfo = new RoutineModel();
@@ -102,7 +128,7 @@ namespace PJCAdmin.ControllersAPI
 
 
             Routine temp = helper.getRoutineAssignedToByName(routineTitle);
-            routineInfo = (RoutineModel)temp;
+            routineInfo = (RoutineModel) temp;
             return routineInfo;
         }
     }
